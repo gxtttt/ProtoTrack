d7d137e081c35896595cb211724bfa0cdaf864f2
gxtttt <127832339+gxtttt@users.noreply.github.com>
2026-04-24 21:34:20 +0800

    Add files via upload

---
+++ lib/train/admin/local.py
@@ -0,0 +1,26 @@
+class EnvironmentSettings:
+    def __init__(self):
+        self.workspace_dir = '/root/xt/Prototrack2'    # Base directory for saving network checkpoints.
+        self.tensorboard_dir = '/root/xt/Prototrack2/tensorboard'    # Directory for tensorboard files.
+        self.pretrained_networks = '/root/xt/Prototrack2/pretrained_networks'
+        self.lasot_dir = '/root/xt/Prototrack2/data/lasot'
+        self.got10k_dir = '/root/xt/Prototrack2/data/got10k/train'
+        self.got10k_val_dir = '/root/xt/Prototrack2/data/got10k/val'
+        self.lasot_lmdb_dir = '/root/xt/Prototrack2/data/lasot_lmdb'
+        self.got10k_lmdb_dir = '/root/xt/Prototrack2/data/got10k_lmdb'
+        self.trackingnet_dir = '/root/xt/Prototrack2/data/trackingnet'
+        self.trackingnet_lmdb_dir = '/root/xt/Prototrack2/data/trackingnet_lmdb'
+        self.coco_dir = '/root/xt/Prototrack2/data/coco'
+        self.coco_lmdb_dir = '/root/xt/Prototrack2/data/coco_lmdb'
+        self.lvis_dir = ''
+        self.sbd_dir = ''
+        self.imagenet_dir = '/root/xt/Prototrack2/data/vid'
+        self.imagenet_lmdb_dir = '/root/xt/Prototrack2/data/vid_lmdb'
+        self.lasher_train_dir = '/root/xt/Prototrack2/data/lasher/trainingset'
+        self.lasher_test_dir = '/root/xt/Prototrack2/data/lasher/testingset'
+        self.imagenetdet_dir = ''
+        self.ecssd_dir = ''
+        self.hkuis_dir = ''
+        self.msra10k_dir = ''
+        self.davis_dir = ''
+        self.youtubevos_dir = ''
